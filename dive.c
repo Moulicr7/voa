21467657ce8e2b40000a094393c622605f134754
Moulicr7 <113498916+Moulicr7@users.noreply.github.com>
2022-09-14 14:47:16 +0530

    Add files via upload

---
+++ dive.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int b, h;
+    float area;
+    printf("ENTER BASE OF TRIANGLE");
+    scanf("%d",&b);
+    printf("\nENTER HEIGHTH OF TRIANGLE");
+    scanf("%d", &h);
+    area=(b *h)/2;
+    printf("\n AREA OF TRIANGLE= %F,AREA");
+    return 0;
+
+}
